5f1621d112926521ce2f1ffb1bf9aedd85b4f867
神戸隆太 <kambe3141@gmail.com>
2019-11-12 12:03:00 +0900

    add simple test

--- model/simple.cpp
+++ model/simple.cpp
@@ -14,8 +14,7 @@ int max_level = 20;
 int test_level = 10;
 int n_sample = 20000;
 
-normal_distribution<double> x_dist(0.0, 1.0);
-normal_distribution<double> y_dist(0.0, 1.0);
+normal_distribution<double> dist(0.0, 1.0);
 
 void sampling_init(EvppiInfo *info) {
     info->model_num = 2;
@@ -24,26 +23,24 @@ void sampling_init(EvppiInfo *info) {
 }
 
 void pre_sampling(EvppiInfo *info) {
-    info->sample[0] = x_dist(generator);
+    double r = dist(generator);
+    info->sample[0] = r >= 0.0 ? 1 : -1;
 }
 
 void post_sampling(EvppiInfo *info) {
-    info->sample[1] = y_dist(generator);
+    double r = dist(generator);
+    info->sample[1] = r >= 0.0 ? 1 : -1;
 }
 
 void f(EvppiInfo *info) {
-    info->val[0] = 0.0;
-    info->val[1] = info->sample[0] + info->sample[1];
+    info->val[0] = info->sample[0] * info->sample[1];
+    info->val[1] = - info->sample[0] * info->sample[1];
 }
 
 int main() {
     MlmcInfo *info = mlmc_init(m0, s, max_level, 1.0, 0.25);
+    smc_evpi_calc(info->layer[0].evppi_info, 10000000);
     mlmc_test(info, test_level, n_sample);
-
-    // smc_evpi_calc(info->layer[0].evppi_info, 10000000);
-
-    vector <double> eps = {0.002, 0.001, 0.0005, 0.0002, 0.0001};
-    mlmc_test_eval_eps(info, eps);
 
     return 0;
 }
