e42404b21ba9e7a7b6e47031ea6a78df1b2b1cc3
神戸隆太 <kambe3141@gmail.com>
2020-01-17 13:57:36 +0900

    update ssi

--- ssi/ssi.cpp
+++ ssi/ssi.cpp
@@ -11,10 +11,6 @@ using namespace std;
 random_device rd;
 mt19937 generator(rd());
 
-int wtp = 20000;
-double ssi_qaly_loss = 0.12;
-vector <double> dressing_cost(4);
-
 normal_distribution<double> dist_normal(0.0, 1.0);
 
 double ssi_cost_mean = 8.972237608;
@@ -29,15 +25,14 @@ vector <double> u(3);
 Matrix sigma(3, 3);
 
 /*
- * わかりにくくてすまん...構造体をうまく使いたい...
- * sample[0~3]: pSSI
- * sample[4]: SSIcost
- * sample[5~7]: d
+ * sample[0]: pSSI2
+ * sample[1]: SSIcost
+ * sample[2~4]: d
  */
 
 void sampling_init(EvppiInfo *info) {
     info->model_num = 4;
-    info->sample.resize(8);
+    info->sample.resize(5);
     info->val.resize(info->model_num);
 }
 
@@ -48,30 +43,33 @@ void pre_sampling(EvppiInfo *info) {
     rand[2] = dist_normal(generator);
     rand_multinormal(u, sigma, rand, ret);
 
-    info->sample[5] = ret[0];
-    info->sample[6] = ret[1];
-    info->sample[7] = ret[2];
+    info->sample[2] = ret[0];
+    info->sample[3] = ret[1];
+    info->sample[4] = ret[2];
 }
 
 void post_sampling(EvppiInfo *info) {
-    info->sample[1] = ssi_risk(generator);
-    info->sample[4] = ssi_cost(generator);
+    info->sample[0] = ssi_risk(generator);
+    info->sample[1] = ssi_cost(generator);
 }
 
 void f(EvppiInfo *info) {
-    info->sample[0] = expit(logit(info->sample[1]) + info->sample[5]);
-    info->sample[2] = expit(logit(info->sample[1]) + info->sample[6]);
-    info->sample[3] = expit(logit(info->sample[1]) + info->sample[7]);
+    int wtp = 20000;
+    double ssi_qaly_loss = 0.12;
+    vector <double> dressing_cost = {0.0, 5.25, 13.86, 21.39};
 
-    info->val[0] = -dressing_cost[0] - info->sample[0] * (info->sample[4] + ssi_qaly_loss * wtp);
-    info->val[1] = -dressing_cost[1] - info->sample[1] * (info->sample[4] + ssi_qaly_loss * wtp);
-    info->val[2] = -dressing_cost[2] - info->sample[2] * (info->sample[4] + ssi_qaly_loss * wtp);
-    info->val[3] = -dressing_cost[3] - info->sample[3] * (info->sample[4] + ssi_qaly_loss * wtp);
+    double pSSI1 = expit(logit(info->sample[0]) + info->sample[2]);
+    double pSSI2 = info->sample[0];
+    double pSSI3 = expit(logit(info->sample[0]) + info->sample[3]);
+    double pSSI4 = expit(logit(info->sample[0]) + info->sample[4]);
+
+    info->val[0] = -dressing_cost[0] - pSSI1 * (info->sample[1] + ssi_qaly_loss * wtp);
+    info->val[1] = -dressing_cost[1] - pSSI2 * (info->sample[1] + ssi_qaly_loss * wtp);
+    info->val[2] = -dressing_cost[2] - pSSI3 * (info->sample[1] + ssi_qaly_loss * wtp);
+    info->val[3] = -dressing_cost[3] - pSSI4 * (info->sample[1] + ssi_qaly_loss * wtp);
 }
 
 int main() {
-    dressing_cost = {0.0, 5.25, 13.86, 21.39};
-
     // NMA_all.txt
     u = {-0.05021921904305, -0.06629096195095, -0.178047360396};
     sigma[0] = {0.06546909, 0.06274766, 0.01781241};
@@ -87,8 +85,8 @@ int main() {
     sigma = Cholesky(sigma);
 
     MlmcInfo *info = mlmc_init(1, 2, 20, 1.0, 0.25);
-    // smc_evpi_calc(info->layer[0].evppi_info, 10000000);
-    mlmc_test(info, 10, 20000);
+    smc_evpi_calc(info->layer[0].evppi_info, 10000000);
+    mlmc_test(info, 10, 200000);
 
     vector <double> eps = {0.0005, 0.0002, 0.0001, 0.00005, 0.00002, 0.00001};
     mlmc_test_eval_eps(info, eps);
