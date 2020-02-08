99640fdf7673b919a848e7ff0a5bb0a6058aa8cf
神戸隆太 <kambe3141@gmail.com>
2020-02-08 18:05:52 +0900

    update chemotherapy

--- chemotherapy/chemotherapy.cpp
+++ chemotherapy/chemotherapy.cpp
@@ -17,10 +17,6 @@ lognormal_distribution<double> dist_cost_ambulatory(7.74, 0.039);
 lognormal_distribution<double> dist_cost_hospital(8.77, 0.15);
 lognormal_distribution<double> dist_cost_death(8.33, 0.13);
 
-vector<double> qaly_of_states(4);
-vector<double> cost_of_states(4);
-Matrix transition(4, 4);
-
 double beta(double alpha, double beta) {
     gamma_distribution<double> dist_gamma1(alpha, 1.0);
     gamma_distribution<double> dist_gamma2(beta, 1.0);
@@ -29,112 +25,93 @@ double beta(double alpha, double beta) {
     return r1 / (r1 + r2);
 }
 
-/*
- * info->sample[0]  : probability of adverse events
- * info->sample[1]  : reduction in adverse events with treatment
- * info->sample[2]  : QoL weight for home care (ambulatory)
- * info->sample[3]  : QoL weight for hospitalization
- * info->sample[4]  : QoL weight with no adverse events
- * info->sample[5]  : cost of home care (ambulatory)
- * info->sample[6]  : cost of hospitalization
- * info->sample[7]  : cost of death
- * info->sample[8]  : probability of hospitalization
- * info->sample[9]  : daily probability of recovery from home care (ambulatory)
- * info->sample[10] : probability of death
- * info->sample[11] : daily probability of recovery from hospital
- */
+struct ModelInfo {
+    double pi0, rho;
+    double q1, q2, q3;
+    double c1, c2, c4;
+    double gamma1, gamma2;
+    double lambda1, lambda2;
+};
 
 void sampling_init(EvppiInfo *info) {
     info->model_num = 2;
-    info->sample.resize(12);
+    info->model_info = new ModelInfo;
     info->val.resize(info->model_num);
 }
 
-void pre_sampling(EvppiInfo *info) {
-    info->sample[0] = beta(28, 85);
-    info->sample[1] = info->sample[0] * dist_rho(generator);
-    info->sample[8] = beta(11, 18);
-    info->sample[9] = beta(5.12, 6.26);
-    info->sample[10] = beta(2, 17);
-    info->sample[11] = beta(3.63, 6.74);
+void pre_sampling(ModelInfo *model) {
+    model->pi0 = beta(28, 85);
+    model->rho = dist_rho(generator);
+    model->gamma1 = beta(11, 18) / 15;
+    model->gamma2 = beta(2, 17) / 15;
+    model->lambda1 = beta(5.12, 6.26);
+    model->lambda2 = beta(3.63, 6.74);
 }
 
-void post_sampling(EvppiInfo *info) {
-    info->sample[2] = beta(5.75, 5.75);
-    info->sample[3] = beta(0.87, 3.47);
-    info->sample[4] = beta(18.23, 0.372);
-    info->sample[5] = dist_cost_ambulatory(generator);
-    info->sample[6] = dist_cost_hospital(generator);
-    info->sample[7] = dist_cost_death(generator);
+void post_sampling(ModelInfo *model) {
+    model->q1 = beta(5.75, 5.75);
+    model->q2 = beta(0.87, 3.47);
+    model->q3 = beta(18.23, 0.372);
+    model->c1 = dist_cost_ambulatory(generator);
+    model->c2 = dist_cost_hospital(generator);
+    model->c4 = dist_cost_death(generator);
 }
 
 void f(EvppiInfo *info) {
-    int threshold = 15;
-    int wtp = 30000;
+    ModelInfo *model = info->model_info;
 
-    qaly_of_states[0] = info->sample[2];
-    qaly_of_states[1] = info->sample[3];
-    qaly_of_states[2] = info->sample[4];
+    vector <double> qaly = {model->q1, model->q2, model->q3, 0};
+    vector <double> cost = {model->c1, model->c2, 0, model->c4};
 
-    cost_of_states[0] = info->sample[5];
-    cost_of_states[1] = info->sample[6];
-    cost_of_states[3] = info->sample[7];
+    Matrix transition(4, 4);
 
-    transition[0][1] = info->sample[8] / threshold;                 // ambulatory -> hospital
-    transition[0][2] = info->sample[9] * (1.0 - transition[0][1]);  // ambulatory -> recovery
-    transition[0][0] = 1.0 - transition[0][1] - transition[0][2];   // stay ambulatory
+    transition[0][0] = (1 - model->lambda1) * (1 - model->gamma1);
+    transition[0][1] = model->gamma1;
+    transition[0][2] = model->lambda1 * (1 - model->gamma1);
+    transition[0][3] = 0;
 
-    transition[1][3] = info->sample[10] / threshold;                // hospital -> death
-    transition[1][2] = info->sample[11] * (1.0 - transition[1][3]); // hospital -> recovery
-    transition[1][1] = 1.0 - transition[1][2] - transition[1][3];   // stay hospital
+    transition[1][0] = 0;
+    transition[1][1] = (1 - model->lambda2) * (1 - model->gamma2);
+    transition[1][2] = model->lambda2 * (1 - model->gamma2);
+    transition[1][3] = model->gamma2;
+
+    transition[2] = {0, 0, 1, 0};
+    transition[3] = {0, 0, 0, 1};
 
     vector <vector<double>> init(2);
-    init[0] = {info->sample[0], 0.0, 0.0, 0.0};
-    init[1] = {info->sample[1], 0.0, 0.0, 0.0};
+    init[0] = {model->pi0, 0.0, 0.0, 0.0};
+    init[1] = {model->pi0 * model->rho, 0.0, 0.0, 0.0};
 
-    vector<double> sum_of_effect(2);
+    vector<double> sum_of_qaly(2), sum_of_cost(2);;
     for (int i = 0; i < 2; i++) {
-        sum_of_effect[i] += qaly_of_states * init[i];
-        for (int j = 0; j < threshold; j++) {
+        sum_of_qaly[i] += qaly * init[i];
+        sum_of_cost[i] += cost * init[i];
+
+        for (int j = 0; j < 15; j++) {
             init[i] = init[i] * transition;
-            sum_of_effect[i] += qaly_of_states * init[i];
+            sum_of_qaly[i] += qaly * init[i];
+            sum_of_cost[i] += cost * init[i];
         }
-        sum_of_effect[i] /= threshold + 1;
+
+        sum_of_qaly[i] /= 16;
+        sum_of_cost[i] /= 16;
     }
 
-    init[0] = {info->sample[0], 0.0, 0.0, 0.0};
-    init[1] = {info->sample[1], 0.0, 0.0, 0.0};
+    sum_of_qaly[0] += (1 - model->pi0) * model->q3;
+    sum_of_qaly[1] += (1 - model->pi0 * model->rho) * model->q3;
 
-    vector<double> sum_of_cost(2);
+    sum_of_cost[0] += 110;
+    sum_of_cost[1] += 420;
+
+    int wtp = 30000;
     for (int i = 0; i < 2; i++) {
-        sum_of_cost[i] += cost_of_states * init[i];
-        for (int j = 0; j < threshold; j++) {
-            init[i] = init[i] * transition;
-            sum_of_cost[i] += cost_of_states * init[i];
-        }
-        sum_of_cost[i] /= threshold + 1;
-    }
-
-    vector <double> cost_of_drug = {110, 420};
-
-    for (int i = 0; i < 2; i++) {
-        info->val[i] =
-                (sum_of_effect[i] + (1.0 - info->sample[i]) * qaly_of_states[2]) * wtp
-                - (sum_of_cost[i] + cost_of_drug[i]);
+        info->val[i] = sum_of_qaly[i] * wtp - sum_of_cost[i];
     }
 }
 
 int main() {
-    qaly_of_states[3] = 0.0;
-    cost_of_states[2] = 0.0;
-
-    transition[0][3] = 0.0;
-    transition[1][0] = 0.0;
-    transition[2] = {0.0, 0.0, 1.0, 0.0};
-    transition[3] = {0.0, 0.0, 0.0, 1.0};
-
     MlmcInfo *info = mlmc_init(1, 2, 20, 1.0, 0.25);
-    // smc_evpi_calc(info->layer[0].evppi_info, 10000000); // 33.09
+    smc_evpi_calc(info->layer[0].evppi_info, 1000000); // 33.09
     mlmc_test(info, 10, 20000);
 
     vector <double> eps = {0.2, 0.1, 0.05, 0.02, 0.01};
